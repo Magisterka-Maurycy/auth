915a79d77cd315eb3f2d1386eb023fcb3aa6caaa
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-06-25 18:14:23 +0200

    IT tests fixes

--- src/native-test/kotlin/org/maurycy/framework/auth/resource/AccessCheckResourceTestIT.kt
+++ src/native-test/kotlin/org/maurycy/framework/auth/resource/AccessCheckResourceTestIT.kt
@@ -1,6 +1,9 @@
 package org.maurycy.framework.auth.resource
 
+import io.quarkus.test.common.QuarkusTestResource
 import io.quarkus.test.junit.QuarkusIntegrationTest
+import org.maurycy.framework.auth.resource.util.KeycloakTestResourceLifecycleManager
 
 @QuarkusIntegrationTest
+@QuarkusTestResource(KeycloakTestResourceLifecycleManager::class)
 class AccessCheckResourceTestIT : AccessCheckResourceTest()
